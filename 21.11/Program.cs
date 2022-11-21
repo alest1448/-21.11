54a6d6eced1319c8a71cb298d2656375df98c1f6
alest1448 <118421044+alest1448@users.noreply.github.com>
2022-11-21 16:01:30 +0900

    Initial commit

---
+++ 21.11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using help;
+
+namespace _21._11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 func = new Class1();
+            Console.WriteLine("Введите имя");
+            func.Hello(Console.ReadLine());
+            Console.WriteLine("Введите возраст");
+            Console.WriteLine($"Через 18 лет вам будет {func.Age(Convert.ToInt32(Console.ReadLine()))}");
+        }
+    }
+}
